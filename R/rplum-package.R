fee018c86b34d39325eb8961d7eec64581726991
Maarten14C <maarten.blaauw@qub.ac.uk>
2025-04-27 10:28:25 +0100

    fixing draw.pbmodelled and agedepth functions from rbacon

--- R/rplum-package.R
+++ R/rplum-package.R
@@ -8,8 +8,8 @@
 #' @docType package
 #' @author Maarten Blaauw <maarten.blaauw@qub.ac.uk> J. Andres Christen <jac@cimat.mx> Marco Aquino-Lopez <aquino@cimat.mx>
 #' @importFrom grDevices dev.off pdf dev.copy2pdf dev.list extendrange dev.cur grey rgb
-#' @importFrom graphics layout legend par plot rect abline axis box image lines mtext text
-#' @importFrom stats lm coef approx density quantile
+#' @importFrom graphics layout legend par plot rect abline axis box image lines mtext text points polygon segments
+#' @importFrom stats lm coef approx density quantile dgamma var
 #' @importFrom utils read.table write.table packageName
 #' @import rbacon
 #' @importFrom rice F14CtoC14 pMCtoC14
